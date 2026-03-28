923f47df8378d4d02c1df113e57fe89abb3daf71
SUMIT <rais74184@gmail.com>
2026-03-28 15:53:13 +0530

    central-1

--- api-gateway/src/main/java/com/app/apigateways/filter/JwtFilter.java
+++ api-gateway/src/main/java/com/app/apigateways/filter/JwtFilter.java
@@ -1,4 +1,29 @@
 package com.app.apigateways.filter;
 
-public class JwtFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtFilter implements GatewayFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain){
+        String token = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if(token == null || !token.startsWith("Bearer ")){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+
+        // call auth-service
+        return chain.filter(exchange);
+    }
 }
